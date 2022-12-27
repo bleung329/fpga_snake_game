e77e54dedb872b934680aaf348c1c3728258e97c
Brian Leung <brianleung329@gmail.com>
2022-12-27 12:54:02 -0500

    Started moving PS outputs to their own processes.

--- game_logic.vhd
+++ game_logic.vhd
@@ -46,7 +46,7 @@ end game_logic;
 
 architecture architecture_game_logic of game_logic is
    --Split RESET into RESET_LCD, RESET_GB, RESET_SNAKE, NEW_FOOD
-   type state_type is (IDLE, RESET_LCD, RESET_GB, RESET_GB_SEND, RESET_SNAKE, RESET_SNAKE_SEND, 
+   type state_type is (IDLE, RESET_LCD, RESET_GB, RESET_GB_SEND, RESET_GB_INC, RESET_SNAKE, RESET_SNAKE_SEND, RESET_SNAKE_INC, 
                      MOVE_HEAD, SEND_HEAD, CHECK_HEAD, ERASE_TAIL, 
                      ADD_LENGTH, NEW_FOOD, NEW_FOOD_CHECK, NEW_FOOD_SEND, 
                      DELAY_INPUT, WIN, LOSE);
@@ -56,8 +56,10 @@ architecture architecture_game_logic of game_logic is
    signal time_since_start_of_cycle : integer;
    signal button_pressed : std_logic;
 
-   signal setup_board_index : integer;
+   signal reset_gb_idx : integer;
+   signal reset_snake_idx : integer;
    
+   --Coordinates of everything
    type coord_type is array(0 to 1) of integer;
    type direction_list_type is array(0 to 3) of coord_type;
    constant UP : coord_type := (0,1);
@@ -74,109 +76,206 @@ architecture architecture_game_logic of game_logic is
    signal snake_array : snake_array_type;
    signal snake_length : integer;
 
-   type snake_init_array_type is array(0 to 2) of coord_type;
-   constant snake_init_array : snake_init_array_type := ((3,3),(3,2),(3,1));
+   type snake_reset_array_type is array(0 to 2) of coord_type;
+   constant snake_reset_array : snake_reset_array_type := ((3,3),(3,2),(3,1));
+
 begin
    button_pressed <= button_l xor button_r;
 
    sync_proc: process(clk_in,button_s)
    begin
-   --If the reset button is hit, start the entire reset process.
+   --If the reset button is hit, start the reset process.
    if (button_s = '1') then
       --TODO: Setup reset process
-      setup_board_index <= -1;
+      --Set init snake length
+      snake_length <= 3;
+      --Set initial snake coords
+      snake_reset_for_loop: for i in 0 to 2 loop
+         snake_array(i) <= snake_reset_array(i);
+      end loop snake_reset_for_loop;
+
+      --Set initial indices for game board and snake index
+      reset_gb_idx <= 0;
+      reset_snake_idx <= 0;
       PS <= RESET_LCD;
+
    elsif rising_edge(clk_in) then
       
-      if (PS = RESET_GB) then
-         setup_board_index <= setup_board_index + 1;
-         case expression is
-            when choice =>
-               
-         
-            when others =>
-               
-         
-         end case;
-      end if;
+      case PS is
+         when RESET_GB_INC =>
+            reset_gb_idx <= reset_gb_idx + 1;
 
-      if (PS = MOVE_HEAD) then
-         --Shift the array to the right 
-         snake_shift_loop: for i in 1 to snake_length loop
-            snake_array(i+1) <= snake_array(i);
-         end loop snake_shift_loop;
-         --Calculate new head
-         snake_array(0)(0) <= snake_array(0)(0) + direction(0);
-         snake_array(0)(1) <= snake_array(0)(1) + direction(1);
-      end if;
+         when RESET_SNAKE_INC =>
+            reset_snake_idx <= reset_snake_idx + 1;
 
-      if (PS = ADD_LENGTH) then
-         snake_length <= snake_length + 1;
-      end if;
+         when MOVE_HEAD =>
+            --Shift the array to the right 
+            snake_shift_loop: for i in 1 to snake_length loop
+               snake_array(i+1) <= snake_array(i);
+            end loop snake_shift_loop;
+            --Calculate new head
+            snake_array(0)(0) <= snake_array(0)(0) + direction(0);
+            snake_array(0)(1) <= snake_array(0)(1) + direction(1);
 
-      if (PS = DELAY_INPUT) then
-         time_since_start_of_cycle <= time_since_start_of_cycle + 1;
-      else
-         time_since_start_of_cycle <= 0;
-      end if;
+         when ADD_LENGTH =>
+            snake_length <= snake_length + 1;
+         when DELAY_INPUT =>
+            time_since_start_of_cycle <= time_since_start_of_cycle + 1;
+         when others =>
+            time_since_start_of_cycle <= 0;
+      end case;
 
       -- Only move to the next state if the lcd is finished drawing/setting up/etc.
       if (lcd_ready_in = '1') then
          PS <= NS;
       end if;
+
    end if;
    end process sync_proc;
 
-   comb_proc : process(PS)
+   color_draw_out_proc: process(PS)
+   begin
+      case PS is
+         when MOVE_HEAD =>
+            color_draw_out <= "01";
+         when SEND_HEAD =>
+            color_draw_out <= "01";
+         when CHECK_HEAD =>
+            color_draw_out <= "11";
+         when ERASE_TAIL =>
+            color_draw_out <= "11";
+         when NEW_FOOD_CHECK =>
+            color_draw_out <= "00";
+         when NEW_FOOD_SEND =>
+            color_draw_out <= "00";
+         when NEW_FOOD =>
+            color_draw_out <= "00"; 
+         when others =>
+            color_draw_out <= "00";
+      end case;
+   end process color_draw_out_proc;
+
+   lcd_reset_out_proc: process(PS)
+   begin
+      case PS is
+         when RESET_LCD =>
+            lcd_reset_out <= '1';
+         when others =>
+            lcd_reset_out <= '0';
+      end case;
+   end process lcd_reset_out_proc;
+
+   send_draw_out_proc: process(PS)
+   begin
+      case PS is
+         when RESET_GB_SEND => 
+            send_draw_out <= '1';
+         when RESET_SNAKE_SEND =>
+            send_draw_out <= '1';
+         when HEAD_SEND =>
+            send_draw_out <= '1';
+         when ERASE_TAIL =>
+            send_draw_out <= '1';
+         when NEW_FOOD_SEND =>
+            send_draw_out <= '1';
+         when others =>
+            send_draw_out <= '0';
+      end case;
+   end process send_draw_out_proc
+
+   new_food_out_proc: process(PS)
+   begin
+      case PS is
+         when NEW_FOOD =>
+            new_food_out <= '1';
+         when others =>
+            new_food_out <= '0';
+      end case;
+   end process new_food_out_proc;
+
+   draw_coord_proc: process(PS)
+   begin
+      case PS is
+         when RESET_GB =>
+            x_draw_out <= conv_unsigned()
+            y_draw_out <= conv_unsigned()
+         when RESET_GB_SEND =>
+            x_draw_out <= conv_unsigned()
+            y_draw_out <= conv_unsigned()
+         when RESET_SNAKE =>
+            x_draw_out <= conv_unsigned(snake_array(reset_snake_idx)(0),8);
+            y_draw_out <= conv_unsigned(snake_array(reset_snake_idx)(1),8);
+         when RESET_SNAKE_SEND =>
+            x_draw_out <= conv_unsigned(snake_array(reset_snake_idx)(0),8);
+            y_draw_out <= conv_unsigned(snake_array(reset_snake_idx)(1),8);
+         when MOVE_HEAD =>
+            x_draw_out <= conv_unsigned(snake_array(0)(0),8);
+            y_draw_out <= conv_unsigned(snake_array(0)(1),8);
+         when SEND_HEAD =>
+            x_draw_out <= conv_unsigned(snake_array(0)(0),8);
+            y_draw_out <= conv_unsigned(snake_array(0)(1),8);
+         when CHECK_HEAD =>
+            x_draw_out <= conv_unsigned(snake_array(snake_length-1)(0),8);
+            y_draw_out <= conv_unsigned(snake_array(snake_length-1)(1),8);
+         when ERASE_TAIL =>
+            x_draw_out <= conv_unsigned(snake_array(snake_length-1)(0),8);
+            y_draw_out <= conv_unsigned(snake_array(snake_length-1)(1),8);
+         when NEW_FOOD_CHECK =>
+            x_draw_out <= food_x_in;
+            y_draw_out <= food_y_in;
+         when NEW_FOOD_SEND =>
+            x_draw_out <= food_x_in;
+            y_draw_out <= food_y_in;
+         when others =>
+            x_draw_out <= x"00";
+            y_draw_out <= x"00";
+      end case;
+   end process draw_coord_proc;
+
+  new_cycle_proc: process(sensitivity_list)
+  begin
+   
+  end process new_cycle_proc;
+
+   NS_comb_proc : process(PS)
       variable snake_hit : std_logic := '0';
       variable food_hit : std_logic := '0';
    begin
       case PS is
          when IDLE =>
-            send_draw_out <= '0';
             NS <= IDLE;
          when RESET_LCD =>
-            send_draw_out <= '0';
-            lcd_reset_out <= '1';
             NS <= RESET_GB;
          when RESET_GB =>
-            send_draw_out <= '0';
-            lcd_reset_out <= '0'; 
-            NS <= RESET_GB_SEND;
+            if (reset_gb_idx > 383) then
+               NS <= RESET_SNAKE;
+            else
+               NS <= RESET_GB_SEND;
+            end if;
          when RESET_GB_SEND =>
-            send_draw_out <= '1';
-            lcd_reset_out <= '0';
+            NS <= RESET_GB_INC;
+         when RESET_GB_INC =>
             NS <= RESET_GB;
          when RESET_SNAKE =>
             reset_dir <= '1';
-            lcd_reset_out <= '0';
-            NS <= RESET_SNAKE_SEND;
+            if (reset_snake_idx > snake_length) then
+               NS <= NEW_FOOD;
+            else
+               NS <= RESET_SNAKE_SEND;
+            end if;
          when RESET_SNAKE_SEND =>
             send_draw_out <= '1';
             lcd_reset_out <= '0';
-            NS <= RESET_GB;
-            NS <= NEW_FOOD;
+            NS <= RESET_SNAKE_INC;
+         when RESET_SNAKE_INC =>
+            NS <= RESET_SNAKE;
          when MOVE_HEAD =>
             reset_dir <= '0';
             direction_changed <= '0';
-            send_draw_out <= '0';
-            --Send new head to the draw output
-            x_draw_out <= conv_unsigned(snake_array(0)(0),8);
-            y_draw_out <= conv_unsigned(snake_array(0)(1),8);
-            -- Draw it green
-            color_draw_out <= "01";
             NS <= SEND_HEAD;
          when SEND_HEAD =>
-            send_draw_out <= '1';
             NS <= CHECK_HEAD;
          when CHECK_HEAD =>
-            send_draw_out <= '0';
-            --Setup the coordinate output to erase the tail
-            x_draw_out <= conv_unsigned(snake_array(snake_length-1)(0),8);
-            y_draw_out <= conv_unsigned(snake_array(snake_length-1)(1),8);
-            --Black, technically
-            color_draw_out <= "11";
-
             --Check if there are collisions
             snake_hit := '0';
             snake_collision_check_loop : for i in 1 to snake_length-1 loop
@@ -204,7 +303,6 @@ begin
             end if;
 
          when ERASE_TAIL =>
-            send_draw_out <= '1';
             NS <= DELAY_INPUT;
          
          when ADD_LENGTH =>
@@ -212,17 +310,12 @@ begin
             NS <= NEW_FOOD;
 
          when NEW_FOOD =>
-            new_food_out <= '1';
             NS <= NEW_FOOD_CHECK;
 
          when NEW_FOOD_CHECK =>
-            new_food_out <= '0';
-            
             food_coord(0) <= conv_integer(food_x_in);
             food_coord(1) <= conv_integer(food_y_in);
-            x_draw_out <= food_x_in;
-            y_draw_out <= food_y_in;
-            
+
             --Ensure the new food placement doesnt land on a snake part. Else, ask for a new one.
             food_hit := '0';
             food_collision_check_loop : for i in 0 to snake_length-1 loop
@@ -241,32 +334,30 @@ begin
             end if;
 
          when NEW_FOOD_SEND =>
-            send_draw_out <= '1';
             NS <= DELAY_INPUT;
 
          when DELAY_INPUT =>
-            send_draw_out <= '0';
             if (time_since_start_of_cycle > time_to_wait) then
                NS <= MOVE_HEAD;
             else
                NS <= DELAY_INPUT;
             end if;
-
          when WIN =>
             NS<=IDLE;
          when LOSE =>
             NS<=IDLE;
          when others =>
-            send_draw_out <= '0';
             NS <= IDLE;
       end case;
    end process comb_proc;
 
-   direction_change_proc: process(PS,button_pressed,reset_dir)
+   direction_change_proc: process(PS,button_pressed,reset_dir,new_cycle)
    variable direction_idx : integer := 0;
    begin
       if (reset_dir = '1') then
          direction_idx := 0;
+      elsif (new_cycle = '1') then
+         direction_changed <= '0';
       elsif (rising_edge(button_pressed) and PS = DELAY_INPUT and direction_changed = '0') then 
          if (button_r = '1') then
             direction_idx := direction_idx + 1;
